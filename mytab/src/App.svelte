e2316bb2ac0c5cf864e9c3bf97165bcfd5cc91eb
tibbsm <tibbs.marc@gmail.com>
2021-03-09 23:12:31 +0900

    basic todo

--- mytab/src/App.svelte
+++ mytab/src/App.svelte
@@ -1,13 +1,49 @@
 <script>
-    let count = 0;
-
-    function sum() {
-        count++;
+    let newItem = '';
+	
+    let todoList = [];
+	
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
+	}
+	
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
     }
+
+	chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
+		console.log('stored...')
+	});
+
+	chrome.storage.sync.get(["yourBody"], function(items){
+		console.log('got', items);
+	});
+
+	chrome.storage.sync.clear(function(){
+		console.log('cleared');
+	});
+	
+	chrome.storage.sync.get(["yourBody"], function(items){
+		console.log('got?', items);
+	});
 </script>
 
-<p>Counter: {count}</p>
-<button on:click={sum}> + </button>
+<input bind:value={newItem} type="text" placeholder="new todo item..">
+<button on:click={addToList}>Add</button>
 
-<style>
-</style>
+<br/>
+{#each todoList as item, index}
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	<span on:click={() => removeFromList(index)}>❌</span>
+	<br/>
+{/each} 
+
+
+<style> 
+	.checked {
+        text-decoration: line-through;
+    }
+</style> 
