342c8fc8b625db081f80cb647ef4943ae1fc17d5
tibbsm <tibbs.marc@gmail.com>
2021-03-13 23:48:24 +0900

    Testing done list options and dev env options

--- src/App.svelte
+++ src/App.svelte
@@ -20,6 +20,8 @@
 		[] 	Storable
 	[] Deal with memory limits on google
 		[] Download | Backup to Drive | ...App
+	[] Add way to dev on this easier?
+		* At least on the functionality part anyways. The storage is a bit more straight forward.
 
  -->
 <script>
@@ -28,35 +30,38 @@
     let todoList = [];
 	let doneList = [];
 	
-	chrome.storage.sync.get(["todos"], function(items){
-		if (items['todos'] && Array.isArray(JSON.parse(items['todos'])))
-			todoList = JSON.parse(items['todos']);
-	});
-	chrome.storage.sync.get(["done"], function(items){
-		if (items['done'] && Array.isArray(JSON.parse(items['done'])))
-			doneList = JSON.parse(items['done']);
-	});
+	// chrome.storage.sync.get(["todos"], function(items){
+	// 	if (items['todos'] && Array.isArray(JSON.parse(items['todos'])))
+	// 		todoList = JSON.parse(items['todos']);
+	// });
+	// chrome.storage.sync.get(["done"], function(items){
+	// 	if (items['done'] && Array.isArray(JSON.parse(items['done'])))
+	// 		doneList = JSON.parse(items['done']);
+	// 	console.log(doneList)
+	// });
 
 	function addToList() {
 		todoList = [...todoList, {text: newItem, status: false}];
 		newItem = '';
-		chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
+		// chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
 	}
 	
 	function removeFromList(index) {
 		todoList.splice(index, 1);
 		todoList = todoList;
-		chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
+		// chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
     }
 
 	function updateList(index) {
 		todoList[index].status = !todoList[index].status;
 		if (todoList[index].status) {
-			doneList.append(todoList[index])
+			doneList.push(todoList[index])
 			todoList.splice(index, 1);
 			todoList = todoList;
+			doneList = doneList;
 		}
-		chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
+		// chrome.storage.sync.set({ "todos": JSON.stringify(todoList) });
+		// chrome.storage.sync.set({ "done": JSON.stringify(doneList) });
 	}
 
 	// chrome.storage.sync.clear(function(){ console.log('cleared') });
@@ -65,6 +70,7 @@
 <input bind:value={newItem} type="text" placeholder="new todo item..">
 <button on:click={addToList}>Add</button>
 
+<h1>To Do</h1>
 <br/>
 {#each todoList as item, index}
 	<input 
@@ -74,6 +80,14 @@
 	>
 	<span class:checked={item.status}>{item.text}</span>
 	<span on:click={() => removeFromList(index)}>❌</span>
+	<br/>
+{/each} 
+
+<br/>
+<h1>Done!</h1>
+<br/>
+{#each doneList as item, index}
+	<span class:checked={item.status}>✅ {item.text}</span>
 	<br/>
 {/each} 
 
