e28aac8f2e7a6acf5accd94c6ecdef6138922c0c
tibbsm <tibbs.marc@gmail.com>
2021-03-22 22:37:08 +0900

    Adding info

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 A simple, customizable tab template for new tabs in chrome. 
 
 TODOs
-[] Done list
-    [] Track todos (github style tracker)
+[] Track todos (github style tracker) for the month
 
 [] Add storage listener
     * chrome.storage.onChanged.addListener(function(changes, namespace) 
@@ -21,21 +20,24 @@ TODOs
 [] Backups
     * Save to Google Drive 
     * Or save it on Dark and risk losing it...
+	[] Store completed task
 
-[] Store completed task
 [] Recurring tasks
 
 [] Notes
-    [] 	Storable
+    [] Storable
 	[] WYSIWYT Editor that saves
 		[] Debounced saves & save button
 	[] Create seperate file for todos, wysiwyt editor, etc..
-	[X] ID number for tasks (object???)
+	
+[] Timestamps
 
 [] Add way to dev on this easier?
     * At least on the functionality part anyways. The storage is a bit more straight forward.
 
 [x] Move todos to README or something
+[x] Done list
+[X] ID number for tasks (object???)
 
 * MDN Svelte Todo 
   * https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_variables_props
--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,29 +1,32 @@
 <script>
+    chrome.storage.sync.get(null, (items) => console.log(items)); // get all
     let newItem = '';
 	
     let todos = [];
-    $: todoList = todos.filter(todo => !todo.status);
-    $: doneList = todos.filter(todo => todo.status);
+    $: todoList = todos.filter(todo => !todo.completed);
+    $: doneList = todos.filter(todo => todo.completed);
 	
     initializeTodos();
 
 	function addToList() {
-		todos.push({
+		todos = [...todos, {
                 id: Date.now().toString(),
                 text: newItem, 
-                status: false
-        });
+                completed: false,
+                created_at: new Date().toDateString(),
+                complete_at: null
+        }];
 		newItem = '';
         saveTodos()
 	}
 	
 	function removeFromList(id) {
 		todos = todos.filter(todo => todo.id !== id);
-		saveTodos()
+		saveTodos();
     }
 
     function saveTodos() {
-        chrome.storage.sync.set({"todos": todos});
+        chrome.storage.sync.set({"todos": JSON.stringify(todos)});
     }
 
     function initializeTodos() {
@@ -34,6 +37,13 @@
                 todos = [];
             }
         });
+    }
+
+    function changeStatus(todo) {
+        todo.completed = !todo.completed;
+        if (!todo.completed) todo.complete_at = null;
+        else todo.complete_at = new Date().toDateString();
+        saveTodos();
     }
 
     // chrome.storage.sync.getBytesInUse(null, (bytesInUse) => console.log(bytesInUse)); // get bytes
@@ -50,31 +60,31 @@
 <input bind:value={newItem} type="text" placeholder="new todo item..">
 <button on:click={addToList}>Add</button>
 
-<h1>To Do</h1>
+<h1>To Do ({todoList.length})</h1>
 {#each todoList as item}
 	<input 
-		on:change={() => item.status = !item.status }
-		bind:checked={item.status} 
+		on:change={() => changeStatus(item)}
+		bind:checked={item.completed} 
 		type="checkbox"
 	>
-	<span class:checked={item.status}>{item.text}</span>
+	<span class:checked={item.completed}>{item.text}</span>
 	<span on:click={() => removeFromList(item.id)}>❌</span>
 	<br/>
 {/each} 
 
-<h1>Done!</h1>
+<h1>Done! ({doneList.length})</h1>
 {#each doneList as item}
 	<input 
-		on:change={() => item.status = !item.status }
-		bind:checked={item.status} 
+        on:change={() => changeStatus(item)}
+		bind:checked={item.completed} 
 		type="checkbox"
 	>
-	<span class:checked={item.status}>{item.text}</span>
+	<span class:checked={item.completed}>{item.text}</span>
 	<span on:click={() => removeFromList(item.id)}>❌</span>
 	<br/>
 {/each} 
 
-<button on:click={() => clear()} disabled>clear</button>
+<button on:click={() => clear()}>clear</button>
 
 <style> 
 	.checked {
