21422a63290a4ed1b2769ad7d9d3944659b4beeb
tibbsm <tibbs.marc@gmail.com>
2021-08-11 20:29:00 +0900

    Prettier for notes file

--- src/Notes.svelte
+++ src/Notes.svelte
@@ -1,49 +1,48 @@
-<script>    
-    import { debounce } from 'lodash-es';
-    
-    let notes = '';
-	
-    initializeNotes();
+<script>
+  import { debounce } from "lodash-es";
 
-    function saveNotes() {
-        chrome.storage.sync.set({"notes": notes});
-    }
+  let notes = "";
 
-    function initializeNotes() {
-        chrome.storage.sync.get(["notes"], function(items){
-            notes = items['notes'] || '';
-        });
-    }
+  initializeNotes();
 
-    function download(content, fileName, contentType) {
-        let a = document.createElement("a");
-        let text = content.replace(/<\/?[^>]+(>|$)/g, "\n");
-        a.href = URL.createObjectURL(new Blob([text], {type: contentType}));
-        a.download = fileName;
-        a.click();
-    }
+  function saveNotes() {
+    chrome.storage.sync.set({ notes: notes });
+  }
+
+  function initializeNotes() {
+    chrome.storage.sync.get(["notes"], function (items) {
+      notes = items["notes"] || "";
+    });
+  }
+
+  function download(content, fileName, contentType) {
+    let a = document.createElement("a");
+    let text = content.replace(/<\/?[^>]+(>|$)/g, "\n");
+    a.href = URL.createObjectURL(new Blob([text], { type: contentType }));
+    a.download = fileName;
+    a.click();
+  }
 </script>
 
-
 <h1>Notes</h1>
-<div 
-    class='notes'
-    bind:innerHTML={notes} 
-    on:keyup={debounce(() => saveNotes(), 250)}
-    contenteditable
-></div>
+<div
+  class="notes"
+  bind:innerHTML={notes}
+  on:keyup={debounce(() => saveNotes(), 250)}
+  contenteditable
+/>
 
-<br>
+<br />
 
-<button
-    on:click={() => download(notes, 'MyTabNotes.txt', 'text/plain')}
->Save notes</button>
+<button on:click={() => download(notes, "MyTabNotes.txt", "text/plain")}
+  >Save notes</button
+>
 
-<style> 
-    [contenteditable] {
-        min-height: 50px;
-        width: 50%;
-        border-radius: 3px;
-        padding: 5px;
-    }
-</style> 
+<style>
+  [contenteditable] {
+    min-height: 50px;
+    width: 50%;
+    border-radius: 3px;
+    padding: 5px;
+  }
+</style>
