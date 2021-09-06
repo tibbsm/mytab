d8173085881516384adab5ad2ea90dbfeab1e23c
tibbsm <tibbs.marc@gmail.com>
2021-09-06 09:21:39 +0900

    improve dummy data

--- src/dummydata.js
+++ src/dummydata.js
@@ -1,46 +1,13 @@
-const dummyTodos = [
-  {
-    id: new Date().getTime(),
-    text: "test 1",
-    completed: false,
+const dummyTodos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => {
+  let completed = Math.random() > 0.5;
+  let complete_at = completed ? new Date().getTime() : null;
+  return {
+    id: new Date().getTime() + Number(id),
+    text: `Todo ${id}`,
     created_at: new Date().getTime(),
-    complete_at: null,
-  },
-  {
-    id: new Date().getTime(),
-    text: "test 2",
-    completed: true,
-    created_at: new Date().getTime() - 60 * 60 * 24 * 3,
-    complete_at: new Date().getTime() - 60 * 60 * 24 * 3,
-  },
-  {
-    id: new Date().getTime(),
-    text: "test 3",
-    completed: false,
-    created_at: new Date().getTime() - 60 * 60 * 24 * 3,
-    complete_at: new Date().getTime() - 60 * 60 * 24 * 3,
-  },
-  {
-    id: new Date().getTime(),
-    text: "test 4",
-    completed: true,
-    created_at: new Date().getTime(),
-    complete_at: null,
-  },
-  {
-    id: new Date().getTime(),
-    text: "test 5",
-    completed: false,
-    created_at: new Date().getTime(),
-    complete_at: null,
-  },
-  {
-    id: new Date().getTime(),
-    text: "test 6`",
-    completed: false,
-    created_at: new Date().getTime(),
-    complete_at: null,
-  },
-];
+    completed,
+    complete_at,
+  };
+});
 
 export default dummyTodos;
