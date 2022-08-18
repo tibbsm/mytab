5835238c5ddf067a395951361f8835f938a01ea4
tibbsm <tibbs.marc@gmail.com>
2022-08-18 20:13:25 +0900

    rm memory

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Time from "./Time.svelte";
-  import Memory from "./Memory.svelte";
   import links from "./links";
   import { debounce } from "lodash-es";
 
+  // FIXME: notes memory is not tracked anymore
+  $: memoryUsed = 0;
+  let hideMemory = true;
   let notes: string;
 
   // XXX fix key logic
@@ -24,7 +26,23 @@
     });
   };
 
+  const getMemoryUsed = () =>
+    chrome.storage?.sync.getBytesInUse(
+      null,
+      (bytesInUse) => (memoryUsed = Math.floor((bytesInUse / 102400) * 100))
+    );
+
+  const initializeMemory = () => {
+    getMemoryUsed();
+    chrome.storage?.onChanged.addListener(() => getMemoryUsed());
+  };
+
+  const getColor = (percent: number): string => {
+    return percent > 90 ? "red" : percent > 70 ? "yellow" : "green";
+  };
+
   initializeNotes();
+  initializeMemory();
 </script>
 
 <svelte:head>
@@ -55,7 +73,23 @@
     on:keyup={debounce(() => saveNotes(), 250)}
     contenteditable
   />
-  <Memory />
+  <div>
+    <button on:click={() => (hideMemory = !hideMemory)}>
+      {hideMemory ? "Show Memory" : "Hide Memory"}
+    </button>
+
+    <div class="memory-wrapper {hideMemory && 'hide'}">
+      <p>Total: ({memoryUsed}%)</p>
+      <div class="meter">
+        <span
+          style={`width: ${memoryUsed}%; background-color: ${getColor(
+            memoryUsed
+          )}`}
+        />
+      </div>
+    </div>
+  </div>
+
   <Time />
 </div>
 
@@ -111,5 +145,30 @@
     border-bottom: 1px solid var(--light);
     background-color: var(--black);
     outline: none;
+  }
+
+  /* Memory */
+  .memory-wrapper p {
+    color: var(--light);
+  }
+  .hide {
+    display: none;
+  }
+  .meter {
+    height: 1em;
+    width: 25%;
+    background: #555;
+    border-radius: 25px;
+    overflow: hidden;
+  }
+  .memory-wrapper .meter > span {
+    position: relative;
+    overflow: hidden;
+    display: block;
+    height: 100%;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
   }
 </style>
