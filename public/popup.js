e0d9a8816c5048f023ae007dda3a7b839519c6ae
tibbsm <tibbs.marc@gmail.com>
2022-12-25 14:02:35 +0900

    add notes for fixes

--- public/popup.js
+++ public/popup.js
@@ -1,9 +1,8 @@
 console.log("popup");
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
   document.getElementById("save").addEventListener("click", function () {
-    console.log("save clicked");
+    console.log("save button clicked");
     var note = document.getElementById("note").value;
     // Save the note to storage
     chrome.storage.local.set({ note });
