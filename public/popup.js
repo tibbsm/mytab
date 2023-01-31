a92bc13c8b8667b0af77c0e20391d2fd2f4c93cc
tibbsm <tibbs.marc@gmail.com>
2023-01-31 13:52:33 +0900

    start work on auto saving notes to storage

--- public/popup.js
+++ public/popup.js
@@ -1,10 +1,20 @@
-console.log("popup");
+// FIXME: file name, not a popup
+console.log("popup.js");
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("save").addEventListener("click", function () {
+  const noteEl = document.getElementById("save");
+  noteEl.addEventListener("click", function () {
     console.log("save button clicked");
     var note = document.getElementById("note").value;
     chrome.storage.local.set({ note });
     chrome.runtime.sendMessage({ saveNotes: true });
   });
+
+  // FIXME: what is the event listener for changes to text/value input
+  // noteEl.addEventListener("click", function () {
+  //   console.log("save button clicked");
+  //   var note = document.getElementById("note").value;
+  //   chrome.storage.local.set({ note });
+  //   chrome.runtime.sendMessage({ saveNotes: true });
+  // });
 });
