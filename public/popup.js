78a72783b7cb3ffe69b7144d1813a85cee502c6e
tibbsm <tibbs.marc@gmail.com>
2023-02-01 16:54:53 +0900

    add input event listener

--- public/popup.js
+++ public/popup.js
@@ -10,11 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.runtime.sendMessage({ saveNotes: true });
   });
 
-  // FIXME: what is the event listener for changes to text/value input
-  // noteEl.addEventListener("click", function () {
-  //   console.log("save button clicked");
+  // FIXME: add debounce
+  // noteEl.addEventListener("input", function () {
+  //   console.log("notes updated");
   //   var note = document.getElementById("note").value;
   //   chrome.storage.local.set({ note });
-  //   chrome.runtime.sendMessage({ saveNotes: true });
   // });
 });
