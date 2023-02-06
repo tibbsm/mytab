7438ddad19072a7104fa1946854246a19f63f57d
tibbsm <tibbs.marc@gmail.com>
2023-02-06 20:41:24 +0900

    add null check in background

--- public/background.js
+++ public/background.js
@@ -1,13 +1,14 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log(request, sender);
 
-  // FIXME: Add null check here on items.note?
   // Save the notes to a file
-  chrome.storage.local.get("note", function (items) {
-    chrome.downloads.download({
-      url: "data:text/plain," + encodeURIComponent(items.note),
-      filename: "notes.txt",
-    });
+  chrome.storage.local.get("note", (items) => {
+    if (items.note != null) {
+      chrome.downloads.download({
+        url: "data:text/plain," + encodeURIComponent(items.note),
+        filename: "notes.txt",
+      });
+    }
   });
 
   sendResponse({ saveNotes: "saved" });
