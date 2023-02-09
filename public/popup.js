e203477d55e81c2c0c48cd1a468711163fc32daf
tibbsm <tibbs.marc@gmail.com>
2023-02-09 20:49:53 +0900

    learnings..

--- public/popup.js
+++ public/popup.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // FIXME: add debounce => lib directory
 // XXX What does args do in this context?
 // XXX how is timer shared between the different functions calls?
+// NOTE: The rest parameter syntax allows a function to accept an indefinite number of arguments as an array,
 function debounce(func, timeout = 300) {
   let timer;
   return (...args) => {
